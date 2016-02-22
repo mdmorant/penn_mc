1cd6687ac88c841720d0cd8d9c65ab21d60a8dc1
Prelang Builder <builder@prelang.com>
2016-02-22 16:49:23 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Payment' to allow for create, update

--- app/admin/payment.rb
+++ app/admin/payment.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Payment do
+  permit_params :amount, :description, :is_deposit
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
