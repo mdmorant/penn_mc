8646c24e1af3915433126dcb3635cfc283b38da1
Prelang Builder <builder@prelang.com>
2016-02-22 16:49:21 +0000

    Installed relation 'has_and_belongs_to_many' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
   has_one :school
+  has_and_belongs_to_many :committees
 end
