bff491c0da961914a70a01a838360107e4f289aa
Prelang Builder <builder@prelang.com>
2016-02-22 16:49:28 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Committee' to allow for create, update

--- app/admin/committee.rb
+++ app/admin/committee.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Committee do
+  permit_params :name, :jpaper, :official_website
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
